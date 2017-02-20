b68c415651a03169716dd38c5e3f48531a756e29
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-02-20 21:42:07 +0100

    Set the indicator state in onResume() (Service might be terminated trough a notification later on)

--- app/src/main/java/com/frostnerd/dnschanger/MainActivity.java
+++ app/src/main/java/com/frostnerd/dnschanger/MainActivity.java
@@ -181,6 +181,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         setIndicatorState(API.checkVPNServiceRunning(this));
     }
 
