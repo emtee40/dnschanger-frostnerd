c1ac4a8989bd08bd854aba242d3d87ef496b2369
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-02-21 17:57:12 +0100

    Added settings activity

---
+++ app/src/main/java/com/frostnerd/dnschanger/SettingsActivity.java
@@ -0,0 +1,21 @@
+package com.frostnerd.dnschanger;
+
+import android.os.Bundle;
+
+import com.frostnerd.utils.preferences.AppCompatPreferenceActivity;
+
+/**
+ * Copyright Daniel Wolf 2017
+ * All rights reserved.
+ * <p>
+ * development@frostnerd.com
+ */
+public class SettingsActivity extends AppCompatPreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
