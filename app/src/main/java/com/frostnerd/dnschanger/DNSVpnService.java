a09430f64762e2652fc0c6891a7fed8b7251e511
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-02-22 18:09:56 +0100

    Don' just copy things... (Preferences keys for Ipv6 were wrong - again)

--- app/src/main/java/com/frostnerd/dnschanger/DNSVpnService.java
+++ app/src/main/java/com/frostnerd/dnschanger/DNSVpnService.java
@@ -92,7 +92,7 @@ public class DNSVpnService extends VpnService {
                         String dns1 = Preferences.getString(DNSVpnService.this, "dns1", "8.8.8.8"),
                                 dns2 = Preferences.getString(DNSVpnService.this, "dns1", "8.8.4.4"),
                         dns1_v6 = Preferences.getString(DNSVpnService.this, "dns1-v6", "2001:4860:4860::8888"),
-                        dns2_v6 = Preferences.getString(DNSVpnService.this, "dns1-v6", "2001:4860:4860::8844");
+                        dns2_v6 = Preferences.getString(DNSVpnService.this, "dns2-v6", "2001:4860:4860::8844");
                         tunnelInterface = builder.setSession("DnsChanger").addAddress("192.168.0.1", 24).addDnsServer(dns1).addDnsServer(dns2)
                                 .addDnsServer(dns1_v6).addDnsServer(dns2_v6).establish();
                         DatagramChannel tunnel = DatagramChannel.open();
