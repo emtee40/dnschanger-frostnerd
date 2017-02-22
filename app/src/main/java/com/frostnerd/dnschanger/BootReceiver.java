6211fee403fb08deb22939f725cc343f4d2ad613
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-02-22 17:12:46 +0100

    Added a BroadcastReceiver to start the VPN when set so in the settings.

---
+++ app/src/main/java/com/frostnerd/dnschanger/BootReceiver.java
@@ -0,0 +1,30 @@
+package com.frostnerd.dnschanger;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.VpnService;
+import android.widget.Toast;
+
+import com.frostnerd.utils.preferences.Preferences;
+
+/**
+ * Copyright Daniel Wolf 2017
+ * All rights reserved.
+ * <p>
+ * development@frostnerd.com
+ */
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) || intent.getAction().equals(Intent.ACTION_LOCKED_BOOT_COMPLETED)){
+            if(Preferences.getBoolean(context,"setting_start_boot",false)){
+                Intent i = VpnService.prepare(context);
+                if(i == null){
+                    context.startService(new Intent(context, DNSVpnService.class).putExtra("start_vpn",true));
+                }
+                else context.startActivity(new Intent(context, BackgroundVpnConfigureActivity.class));
+            }
+        }
+    }
+}
