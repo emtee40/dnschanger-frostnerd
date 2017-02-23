7393248d0f58a8738526d1f726e822599f0e3d30
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-02-23 19:25:54 +0100

    Added a service to start the VPN when set so in the preferences

---
+++ app/src/main/java/com/frostnerd/dnschanger/ConnectivityBackgroundService.java
@@ -0,0 +1,62 @@
+package com.frostnerd.dnschanger;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.VpnService;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.frostnerd.utils.preferences.Preferences;
+
+/**
+ * Copyright Daniel Wolf 2017
+ * All rights reserved.
+ * <p>
+ * development@frostnerd.com
+ */
+public class ConnectivityBackgroundService extends Service {
+    private BroadcastReceiver connectivityChange = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            boolean connected = !intent.hasExtra("noConnectivity");
+            int type = intent.getIntExtra("networkType", -1);
+            if(!connected)return;
+            if(type == ConnectivityManager.TYPE_WIFI && Preferences.getBoolean(ConnectivityBackgroundService.this,"setting_auto_wifi",false)){
+                startService();
+            }else if(type == ConnectivityManager.TYPE_MOBILE && Preferences.getBoolean(ConnectivityBackgroundService.this,"summary_auto_mobile",false)){
+                startService();
+            }
+        }
+    };
+
+    private void startService(){
+        Intent i = VpnService.prepare(this);
+        if(i == null){
+            this.startService(new Intent(this, DNSVpnService.class).putExtra("start_vpn",true));
+        }
+        else this.startActivity(new Intent(this, BackgroundVpnConfigureActivity.class));
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerReceiver(connectivityChange, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        System.out.println("BACKGROUND CONNECTIVITY START");
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
