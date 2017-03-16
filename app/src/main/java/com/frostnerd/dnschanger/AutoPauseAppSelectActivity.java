778a49d395e17c53966aeebd7310bbeb171bf489
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-03-16 22:58:06 +0100

    Added activity for selecting the apps which pause the vpn

---
+++ app/src/main/java/com/frostnerd/dnschanger/AutoPauseAppSelectActivity.java
@@ -0,0 +1,20 @@
+package com.frostnerd.dnschanger;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Copyright Daniel Wolf 2017
+ * All rights reserved.
+ * <p>
+ * development@frostnerd.com
+ */
+public class AutoPauseAppSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autopause_app_select);
+    }
+}
