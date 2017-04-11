df7c45ed7a7cb53013426e48a869779f7e404024
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-04-11 19:34:10 +0200

    Extended Application for global logging

---
+++ app/src/main/java/com/frostnerd/dnschanger/DNSChanger.java
@@ -0,0 +1,47 @@
+package com.frostnerd.dnschanger;
+
+import android.app.Application;
+
+import com.frostnerd.dnschanger.activities.ErrorDialogActivity;
+
+/**
+ * Copyright Daniel Wolf 2017
+ * All rights reserved.
+ * <p>
+ * development@frostnerd.com
+ */
+public class DNSChanger extends Application {
+    private static final String LOG_TAG = "[DNSCHANGER-APPLICATION]";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LogFactory.writeMessage(this, LOG_TAG, "Application created");
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                LogFactory.writeStackTrace(DNSChanger.this, new String[]{LOG_TAG, LogFactory.Tag.ERROR.toString()}, e);
+                ErrorDialogActivity.show(DNSChanger.this, e);
+            }
+        });
+    }
+
+    @Override
+    public void onLowMemory() {
+        LogFactory.writeMessage(this, LOG_TAG, "Application got message about low memory");
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        LogFactory.writeMessage(this, LOG_TAG, "Memory was trimmed. Level: " + level);
+        super.onTrimMemory(level);
+    }
+
+    @Override
+    public void onTerminate() {
+        LogFactory.writeMessage(this, LOG_TAG, "Application terminated");
+        LogFactory.terminate();
+        super.onTerminate();
+    }
+}
