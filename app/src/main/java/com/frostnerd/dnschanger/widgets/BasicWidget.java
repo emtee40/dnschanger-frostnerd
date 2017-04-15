17dc6ff03eeb20d9912adf1c7df2a3317e55fa1a
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-04-16 01:39:52 +0200

    Opemized imports

--- app/src/main/java/com/frostnerd/dnschanger/widgets/BasicWidget.java
+++ app/src/main/java/com/frostnerd/dnschanger/widgets/BasicWidget.java
@@ -4,12 +4,9 @@ import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.ServiceConnection;
-import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.widget.RemoteViews;
@@ -18,7 +15,6 @@ import com.frostnerd.dnschanger.API;
 import com.frostnerd.dnschanger.LogFactory;
 import com.frostnerd.dnschanger.R;
 import com.frostnerd.dnschanger.activities.PinActivity;
-import com.frostnerd.dnschanger.services.DNSVpnService;
 
 /**
  * Copyright Daniel Wolf 2017
