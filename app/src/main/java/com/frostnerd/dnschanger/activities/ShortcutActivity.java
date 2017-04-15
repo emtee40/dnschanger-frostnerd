530ec30083e4d9d033403a706a39460007016f6b
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-04-15 13:12:59 +0200

    Destroy Service to be safe

--- app/src/main/java/com/frostnerd/dnschanger/activities/ShortcutActivity.java
+++ app/src/main/java/com/frostnerd/dnschanger/activities/ShortcutActivity.java
@@ -3,8 +3,11 @@ package com.frostnerd.dnschanger.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.frostnerd.dnschanger.API;
 import com.frostnerd.dnschanger.LogFactory;
+import com.frostnerd.dnschanger.services.DNSVpnService;
 
 /**
  * Copyright Daniel Wolf 2017
@@ -26,6 +29,11 @@ public class ShortcutActivity extends AppCompatActivity {
                 dns1v6 = i.getStringExtra("dns1v6"), dns2v6 = i.getStringExtra("dns2v6");
         LogFactory.writeMessage(this, LOG_TAG, "DNS1: " + dns1 + ", DNS2: " + dns2 + ", DNS1V6: " + dns1v6 + ", DNS2V6: " + dns2v6);
         LogFactory.writeMessage(this, LOG_TAG, "Starting BackgroundVPNConfigureActivity");
+        if(API.checkVPNServiceRunning(this)){
+            LogFactory.writeMessage(this, LOG_TAG, "Destroying service to be safe");
+            startService(new Intent(this, DNSVpnService.class).putExtra("destroy", true));
+            LogFactory.writeMessage(this, LOG_TAG, "Destroy command sent");
+        }else LogFactory.writeMessage(this, LOG_TAG, "Service not running. No need to destroy first");
         BackgroundVpnConfigureActivity.startWithFixedDNS(this,dns1,
                 dns2,dns1v6, dns2v6,false);
         finish();
