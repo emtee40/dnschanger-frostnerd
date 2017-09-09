1b03eb86b3907e383285b4636383affd06125657
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-09-09 16:13:04 +0200

    ErrorDialog is now only shown for specific exceptions

--- app/src/main/java/com/frostnerd/dnschanger/services/DNSVpnService.java
+++ app/src/main/java/com/frostnerd/dnschanger/services/DNSVpnService.java
@@ -442,7 +442,7 @@ public class DNSVpnService extends VpnService {
                 threadRunning = true;
                 LogFactory.writeMessage(DNSVpnService.this, new String[]{LOG_TAG, "[VPNTHREAD]", ID}, "Starting Thread (run)");
                 runThread = true;
-                Thread.setDefaultUncaughtExceptionHandler(((DNSChanger)getApplication()).getExcpetionHandler());
+                Thread.setDefaultUncaughtExceptionHandler(((DNSChanger)getApplication()).getExceptionHandler());
                 if(notificationBuilder != null) notificationBuilder.setWhen(System.currentTimeMillis());
                 boolean ipv6Enabled = API.isIPv6Enabled(DNSVpnService.this),
                         ipv4Enabled = API.isIPv4Enabled(DNSVpnService.this);
