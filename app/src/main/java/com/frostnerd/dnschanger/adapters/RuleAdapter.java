4bd9b59ccb3f059ecdd6e05d8c97edde4c108e76
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-10-15 15:34:04 +0200

    The cursor has to be closed

--- app/src/main/java/com/frostnerd/dnschanger/adapters/RuleAdapter.java
+++ app/src/main/java/com/frostnerd/dnschanger/adapters/RuleAdapter.java
@@ -162,6 +162,7 @@ public class RuleAdapter extends RecyclerView.Adapter<RuleAdapter.ViewHolder>{
                     rawCount++;
                 }while(cursor.moveToNext());
             }
+            cursor.close();
             rowRemapPos += fetchCount;
         }
     }
