7a5698d1d44a08adb18f126fc489e787b2e0665f
Daniel Wolf <daniel.wolf@frostnerd.com>
2017-11-15 22:57:16 +0100

    The resolver needs speed. Speed which isn't possible to achieve with ORM (ORM is at ~8ms per host which is still incredibly fast for a database containing 2 million entries, but the direct way needs ~3ms and has less overhead)

--- app/src/main/java/com/frostnerd/dnschanger/database/accessors/DNSResolver.java
+++ app/src/main/java/com/frostnerd/dnschanger/database/accessors/DNSResolver.java
@@ -1,13 +1,13 @@
 package com.frostnerd.dnschanger.database.accessors;
 
 import android.content.Context;
+import android.database.Cursor;
 
 import com.frostnerd.dnschanger.database.DatabaseHelper;
 import com.frostnerd.dnschanger.database.entities.DNSRule;
 import com.frostnerd.dnschanger.util.Util;
 import com.frostnerd.utils.database.orm.parser.Column;
 import com.frostnerd.utils.database.orm.parser.ParsedEntity;
-import com.frostnerd.utils.database.orm.statementoptions.queryoptions.OrderOption;
 import com.frostnerd.utils.database.orm.statementoptions.queryoptions.WhereCondition;
 
 /**
@@ -19,17 +19,28 @@ import com.frostnerd.utils.database.orm.statementoptions.queryoptions.WhereCondi
 public class DNSResolver {
     private DatabaseHelper db;
     private int wildcardCount;
-    private ParsedEntity<DNSRule> ruleEntity;
-    private Column<DNSRule> targetColumn, hostColumn, ipv6Column, wildcardColumn;
+    private final String WILDCARD_QUERY_RANDOM;
+    private final String WILDCARD_QUERY_FIRST;
+    private final String NON_WILDCARD_QUERY;
+
 
     public DNSResolver(Context context) {
         db = Util.getDBHelper(context);
-        ruleEntity = db.getSQLHandler(DNSRule.class);
-        targetColumn = ruleEntity.getTable().findColumn("target");
-        hostColumn = ruleEntity.getTable().findColumn("host");
-        ipv6Column = ruleEntity.getTable().findColumn("ipv6");
-        wildcardColumn = ruleEntity.getTable().findColumn("wildcard");
+        ParsedEntity<DNSRule> ruleEntity = db.getSQLHandler(DNSRule.class);
+        Column<DNSRule> targetColumn = ruleEntity.getTable().findColumn("target");
+        Column<DNSRule> hostColumn = ruleEntity.getTable().findColumn("host");
+        Column<DNSRule> ipv6Column = ruleEntity.getTable().findColumn("ipv6");
+        Column<DNSRule> wildcardColumn = ruleEntity.getTable().findColumn("wildcard");
         wildcardCount = ruleEntity.getCount(db, WhereCondition.equal(wildcardColumn, "1"));
+        WILDCARD_QUERY_RANDOM = "SELECT " + targetColumn.getColumnName() + " FROM " + ruleEntity.getTableName() +
+                " WHERE " + ipv6Column.getColumnName() + "" + "=? AND " + wildcardColumn.getColumnName()
+                + "=1 AND ? REGEXP " + hostColumn.getColumnName() + " ORDER BY RANDOM() LIMIT 1";
+        WILDCARD_QUERY_FIRST = "SELECT " + targetColumn.getColumnName() + " FROM " + ruleEntity.getTableName() +
+                " WHERE " + ipv6Column.getColumnName() + "" + "=? AND " + wildcardColumn.getColumnName()
+                + "=1 AND ? REGEXP " + hostColumn.getColumnName() + " LIMIT 1";
+        NON_WILDCARD_QUERY = "SELECT " + targetColumn.getColumnName() + " FROM " + ruleEntity.getTableName() +
+                " WHERE " + hostColumn.getColumnName() + "=? AND " + ipv6Column.getColumnName() + "=? AND " +
+                wildcardColumn.getColumnName() + "=0";
     }
 
     public String resolve(String host, boolean ipv6) {
@@ -46,24 +57,24 @@ public class DNSResolver {
     }
 
     public String resolveNonWildcard(String host, boolean ipv6) {
-        return ruleEntity.selectFirstRowValue(db, targetColumn, false,
-                WhereCondition.equal(ipv6Column, ipv6 ? "1" : "0"),
-                WhereCondition.equal(wildcardColumn, "0"),
-                WhereCondition.equal(hostColumn, host));
+        String result = null;
+        Cursor cursor = db.getReadableDatabase().rawQuery(NON_WILDCARD_QUERY,
+                new String[]{host, ipv6 ? "1" : "0"});
+        if (cursor.moveToFirst()) {
+            result = cursor.getString(0);
+        }
+        cursor.close();
+        return result;
     }
 
     public String resolveWildcard(String host, boolean ipv6, boolean matchFirst) {
-        if(matchFirst){
-            return ruleEntity.selectFirstRowValue(db, targetColumn, false,
-                    WhereCondition.equal(ipv6Column, ipv6 ? "1" : "0"),
-                    WhereCondition.equal(wildcardColumn, "0"),
-                    WhereCondition.equal(hostColumn, host));
-        }else{
-            return ruleEntity.selectFirstRowValue(db, targetColumn, false,
-                    WhereCondition.equal(ipv6Column, ipv6 ? "1" : "0"),
-                    WhereCondition.equal(wildcardColumn, "0"),
-                    WhereCondition.equal(hostColumn, host),
-                    new OrderOption(OrderOption.Option.RANDOM));
+        String result = null;
+        Cursor cursor = db.getReadableDatabase().rawQuery(matchFirst ? WILDCARD_QUERY_FIRST : WILDCARD_QUERY_RANDOM,
+                new String[]{ipv6 ? "1" : "0", host});
+        if (cursor.moveToFirst()) {
+            result = cursor.getString(0);
         }
+        cursor.close();
+        return result;
     }
 }
