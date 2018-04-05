8e1f7a2b19ddbc1e69e12ebd6e1f6862ce7d4d01
Daniel Wolf <daniel.wolf@frostnerd.com>
2018-04-05 12:56:05 +0200

    Only create the CreationListener once, dismiss the dialog if the adapter is destroyed

--- app/src/main/java/com/frostnerd/dnschanger/adapters/RuleAdapter.java
+++ app/src/main/java/com/frostnerd/dnschanger/adapters/RuleAdapter.java
@@ -1,6 +1,7 @@
 package com.frostnerd.dnschanger.adapters;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
@@ -15,7 +16,6 @@ import com.frostnerd.dnschanger.database.DatabaseHelper;
 import com.frostnerd.dnschanger.database.entities.DNSRule;
 import com.frostnerd.dnschanger.dialogs.NewRuleDialog;
 import com.frostnerd.dnschanger.util.RuleImport;
-import com.frostnerd.dnschanger.util.Util;
 import com.frostnerd.utils.adapters.DatabaseAdapter;
 import com.frostnerd.utils.database.orm.parser.columns.Column;
 import com.frostnerd.utils.database.orm.statementoptions.queryoptions.WhereCondition;
@@ -32,14 +32,29 @@ import com.frostnerd.utils.database.orm.statementoptions.queryoptions.WhereCondi
  */
 public class RuleAdapter extends DatabaseAdapter<DNSRule, RuleAdapter.ViewHolder>{
     private LayoutInflater layoutInflater;
+    private NewRuleDialog newRuleDialog;
+    private Context context;
     private static Column<DNSRule> ipv6Column;
     private static Column<DNSRule> hostColumn;
     private static Column<DNSRule> targetColumn;
     private static Column<DNSRule> wildcardColumn;
+    private NewRuleDialog.CreationListener creationListener = new NewRuleDialog.CreationListener() {
+        @Override
+        public void creationFinished(@NonNull String host, @Nullable String target, @Nullable String targetV6, boolean ipv6, boolean wildcard, boolean wasEdited) {
+            if (target != null)
+                DatabaseHelper.getInstance(context).editDNSRule(host, ipv6, target);
+            else {
+                DatabaseHelper.getInstance(context).deleteDNSRule(host, ipv6);
+            }
+            newRuleDialog = null;
+            reloadData();
+        }
+    };
 
     public <T extends Activity &RuleImport.ImportStartedListener> RuleAdapter(final T context, DatabaseHelper databaseHelper, final TextView rowCount, ProgressBar updateProgress){
         super(databaseHelper, 10000);
         this.layoutInflater = LayoutInflater.from(context);
+        this.context = context;
         setOnRowLoaded(new OnRowLoaded<DNSRule, ViewHolder>() {
             @Override
             public void bindRow(ViewHolder view, final DNSRule entity) {
@@ -48,17 +63,8 @@ public class RuleAdapter extends DatabaseAdapter<DNSRule, RuleAdapter.ViewHolder
                 view.itemView.setOnLongClickListener(new View.OnLongClickListener() {
                     @Override
                     public boolean onLongClick(View v) {
-                        new NewRuleDialog(context, new NewRuleDialog.CreationListener() {
-                            @Override
-                            public void creationFinished(@NonNull String host, @Nullable String target, @Nullable String targetV6, boolean ipv6, boolean wildcard, boolean editingMode) {
-                                if (target != null)
-                                    DatabaseHelper.getInstance(context).editDNSRule(host, ipv6, target);
-                                else {
-                                    DatabaseHelper.getInstance(context).deleteDNSRule(host, ipv6);
-                                }
-                                reloadData();
-                            }
-                        }, entity.getHost(), entity.getTarget(), entity.isWildcard(), entity.isIpv6()).show();
+                        newRuleDialog = new NewRuleDialog(context, creationListener, entity.getHost(), entity.getTarget(), entity.isWildcard(), entity.isIpv6());
+                        newRuleDialog.show();
                         return true;
                     }
                 });
@@ -111,6 +117,8 @@ public class RuleAdapter extends DatabaseAdapter<DNSRule, RuleAdapter.ViewHolder
     public void cleanup() {
         super.cleanup();
         layoutInflater = null;
+        if(newRuleDialog != null)newRuleDialog.dismiss();
+        newRuleDialog = null;
     }
 
     public enum ArgumentLessFilter implements DatabaseAdapter.ArgumentLessFilter{
