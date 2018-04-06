de233ff3ff281900cb3ac56b7c9b75d1b7e5aa0b
Daniel Wolf <daniel.wolf@frostnerd.com>
2018-04-06 23:53:59 +0200

    Added restrictions for all preference types

--- app/src/main/java/com/frostnerd/dnschanger/util/Preferences.java
+++ app/src/main/java/com/frostnerd/dnschanger/util/Preferences.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 
 import com.frostnerd.utils.preferences.restrictions.PreferenceRestriction;
 import com.frostnerd.utils.preferences.restrictions.PreferencesRestrictionBuilder;
+import com.frostnerd.utils.preferences.restrictions.Type;
 
 /**
  * Copyright Daniel Wolf 2018
@@ -33,27 +34,44 @@ public class Preferences extends com.frostnerd.utils.preferences.Preferences {
 
     private void setRestrictions(){
         PreferencesRestrictionBuilder builder = new PreferencesRestrictionBuilder();
-        builder.key("dns1").shouldNotBe(null).always().shouldNotBe("").always().shouldBeLike(Util.ipv4WithPort).always().doneWithKey();
-        builder.key("dns1-v6").shouldNotBe(null).always().shouldNotBe("").always().shouldBeLike(Util.ipv6WithPort).always().doneWithKey();
-        builder.key("setting_ipv6_enabled").shouldNotBe(false).when(new PreferenceRestriction.Condition() {
+        builder.key("dns1").ofType(Type.STRING).shouldNotBe(null).always().shouldNotBe("").always().shouldBeLike(Util.ipv4WithPort).always().doneWithKey();
+        builder.key("dns1-v6").ofType(Type.STRING).shouldNotBe(null).always().shouldNotBe("").always().shouldBeLike(Util.ipv6WithPort).always().doneWithKey();
+        builder.key("setting_ipv6_enabled").ofType(Type.BOOLEAN).shouldNotBe(false).when(new PreferenceRestriction.Condition() {
             @Override
             public boolean isMet(@NonNull com.frostnerd.utils.preferences.Preferences preferences, @NonNull String key, @Nullable Object newValue) {
                 return !preferences.getBoolean("setting_ipv4_enabled", true);
             }
         }).doneWithKey();
-        builder.key("setting_ipv4_enabled").shouldNotBe(false).when(new PreferenceRestriction.Condition() {
+        builder.key("setting_ipv4_enabled").ofType(Type.BOOLEAN).shouldNotBe(false).when(new PreferenceRestriction.Condition() {
             @Override
             public boolean isMet(@NonNull com.frostnerd.utils.preferences.Preferences preferences, @NonNull String key, @Nullable Object newValue) {
                 return !preferences.getBoolean("setting_ipv6_enabled", true);
             }
         }).doneWithKey();
-        builder.key("setting_show_notification").shouldBe(true).when(new PreferenceRestriction.Condition() {
+        builder.key("setting_show_notification").ofType(Type.BOOLEAN).shouldBe(true).when(new PreferenceRestriction.Condition() {
             @Override
             public boolean isMet(@NonNull com.frostnerd.utils.preferences.Preferences preferences, @NonNull String key, @Nullable Object newValue) {
                 return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
             }
         }).doneWithKey();
-        builder.key("pin_value").shouldNotBe("").always().shouldNotBe(null).always().doneWithKey();
+        builder.key("pin_value").ofType(Type.STRING).shouldNotBe("").always().shouldNotBe(null).always().doneWithKey();
+
+        String[] booleanPreferences = {"hide_notification_icon", "notification_on_stop", "setting_start_boot",
+                "setting_auto_wifi", "setting_auto_mobile", "setting_disable_netchange", "setting_pin_enabled",
+                "pin_fingerprint", "pin_notification", "pin_tile", "pin_app_shortcut", "shortcut_click_again_disable",
+                "excluded_whitelist", "device_admin", "setting_app_shortcuts_enabled", "check_connectivity", "debug",
+                "advanced_settings", "loopback_allowed", "custom_port", "rules_activated", "dns_over_tcp", "query_logging",
+                "first_run", "rated", "auto_pause", "everything_disabled", "app_whitelist_configured", "ipv6_asked",
+                "start_service_when_available"};
+        for(String s: booleanPreferences){
+            builder.key(s).ofType(Type.BOOLEAN).doneWithKey();
+        }
+
+        builder.key("theme").ofType(Type.INTEGER).doneWithKey();
+        builder.key("tcp_timeout").ofType(Type.INTEGER).doneWithKey();
+        builder.key("launches").ofType(Type.INTEGER).doneWithKey();
+        builder.key("autopause_apps_count").ofType(Type.INTEGER).doneWithKey();
+        builder.key("autopause_apps").ofType(Type.ANY_SAVEABLE).doneWithKey();
         restrict(builder.build());
     }
 }
