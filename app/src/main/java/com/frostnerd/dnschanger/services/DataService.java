8c0519ddcf8f99f4dea9b1f554fb0dd2ab77fd0f
Daniel Wolf <daniel.wolf@frostnerd.com>
2018-04-24 15:55:50 +0200

    If arg1 passed with the message is 1 the DNS entry list will now open (and an entry will be selected)

--- app/src/main/java/com/frostnerd/dnschanger/services/DataService.java
+++ app/src/main/java/com/frostnerd/dnschanger/services/DataService.java
@@ -1,13 +1,14 @@
 package com.frostnerd.dnschanger.services;
 
+import android.content.Intent;
 import android.os.Message;
 import android.os.RemoteException;
 
+import com.frostnerd.dnschanger.activities.BackgroundDNSListActivity;
 import com.frostnerd.dnschanger.util.Preferences;
 import com.frostnerd.utils.apis.DataExchangeService;
 import com.frostnerd.utils.apis.DataExchanger;
 import com.frostnerd.utils.apis.dataexchangers.PreferencesExchanger;
-
 /**
  * Copyright Daniel Wolf 2017
  * All rights reserved.
@@ -17,12 +18,19 @@ import com.frostnerd.utils.apis.dataexchangers.PreferencesExchanger;
  * <p>
  * development@frostnerd.com
  */
-public class DataService extends DataExchangeService{
+public class DataService extends DataExchangeService {
+    public static final int ARG_CHOOSE_SERVER = 1;
 
-    public void handleMessage(Message message){
-        if(message.replyTo != null){
+    public void handleMessage(final Message message) {
+        if (message.replyTo != null) {
+            if (message.arg1 == ARG_CHOOSE_SERVER) {
+                startActivity(new Intent(this, BackgroundDNSListActivity.class).
+                        putExtra(BackgroundDNSListActivity.KEY_MESSAGE, message));
+            }
+        } else {
             try {
-                DataExchanger.executeExchangersAndSendAnswers(Preferences.getInstance(this), message, message.replyTo, PreferencesExchanger.class);
+                DataExchanger.executeExchangersAndSendAnswers(Preferences.getInstance(this), message,
+                        message.replyTo, PreferencesExchanger.class);
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
