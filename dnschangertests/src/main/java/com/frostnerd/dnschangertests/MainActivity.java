d091be28f0dd25f72844a77d488fd9c42a855c67
Daniel Wolf <daniel.wolf@frostnerd.com>
2020-05-01 16:56:28 +0200

    Removed test VPN service

--- dnschangertests/src/main/java/com/frostnerd/dnschangertests/MainActivity.java
+++ dnschangertests/src/main/java/com/frostnerd/dnschangertests/MainActivity.java
@@ -11,18 +11,10 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent i = VpnService.prepare(this);
-        if(i != null){
-            startActivityForResult(i, 1);
-        }else startService(new Intent(this, TestVPNService.class));
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Intent i = VpnService.prepare(this);
-        if(i != null){
-            startActivityForResult(i, 1);
-        }else startService(new Intent(this, TestVPNService.class));
     }
 }
